8dedf7ba84bb73ab2db6d81a8b5cd99838feb271
jmassarani <julianma95@gmail.com>
2018-04-26 14:02:34 -0400

    working model

--- model/model.py
+++ model/model.py
@@ -10,7 +10,11 @@ rootdir = 'data/'
 train_dir = rootdir + 'train/'
 test_dir = rootdir + 'test/'
 
-
+rev_idx = {
+    'sober': 0,
+    'euphoria': 1,
+    'coma': 2
+}
 def extract_features_labels(folder, train=True):
     features, labels = None, None
     for file in os.listdir(train_dir + folder):
@@ -30,27 +34,27 @@ def extract_features_labels(folder, train=True):
                 actual_data = list(eval(entry[1:]))
                 gx, gy, gz = actual_data[0], actual_data[1], actual_data[2]
                 magnitude = (gx**2 + gz**2) ** 0.5
-                gyro_acc.append(magnitude)
+                gyro_acc += [magnitude]
             else:  # accel
                 actual_data = list(eval(entry.replace('\ufeff', '')))
                 ax, ay, az = actual_data[0], actual_data[1], actual_data[2]
                 magnitude = (ax**2 + ay**2 + az**2) ** 0.5
-                accel_acc.append(magnitude)
+                accel_acc += [magnitude]
             if WINDOW_SIZE - acc <= 1:
                 accel_mean = np.mean(accel_acc)
                 gyro_mean = np.mean(gyro_acc)
-                new_features.append([accel_mean, gyro_mean])
-                new_labels.append(folder)
+                new_features += [accel_mean, gyro_mean]
+                
+                new_labels +=[rev_idx[folder]]
                 if features is not None:
-                    features.append(new_features)
-                    labels.append(new_labels)
+                    features += [new_features]
+                    labels += (new_labels)
                 else:
                     features = new_features
                     labels = new_labels
                 new_features, new_labels = [], []
                 acc = 0
-        
-    return features, labels
+    return features[2:], labels[1:]
 ###################### End load and Process Data ###########################
 
 ############### Model Stuff ################################
@@ -59,15 +63,23 @@ Y_train = None
 X_test = None 
 Y_test = None 
 for file in os.listdir(train_dir):
-    if file == '.DS_Store':
+    if file == '.DS_Store' or file == 'coma':
         continue
     features, labels = extract_features_labels(file)
-    print(features, labels)
-    # print(features, labels)
-# clf = svm.SVC()
-# X, y = [], []
-# clf.fit(X, y)
-# test_data = np.zeros(shape=(2,2))
+    if X_train is None:
+        if features is not None:
+            X_train = np.array(features)
+            Y_train = np.array(labels)
+    else:
+        X_train = np.append(X_train, features, axis=0)
+        Y_train = np.append(Y_train, labels, axis= 0)
+# print(X_train, Y_train)
+print(X_train)
+print(X_train.shape, Y_train.shape)
+clf = svm.SVC()
+X, y = X_train, Y_train
+clf.fit(X, y)
+
 # clf.predict(test_data)
 ############### End Model Stuff ############################
 # for folder in os.listdir(train_dir):
