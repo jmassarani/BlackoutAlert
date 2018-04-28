76d1107de4e2d058c1e764a97b66849301a0bd97
jmassarani <julianma95@gmail.com>
2018-04-28 15:47:15 -0400

    shit works

--- model/model.py
+++ model/model.py
@@ -16,46 +16,61 @@ rev_idx = {
     'euphoria': 1,
     'coma': 2
 }
-
-def extract_features_labels(folder, train=True):
+def extract_features_labels(folder, task, train=True):
     features, labels = None, None
     for file in os.listdir(train_dir + folder):
-        with open(train_dir + folder + '/' + file, 'r') as f:
-            string_data = f.read()
-
-        list_data = (string_data.split('\n'))
-        list_data = list(filter(None, list_data))
-        new_features, new_labels = [], []
-        acc = 0
-        accel_acc, gyro_acc = [], []
         
-        for entry in list_data:
-            acc += 1
-            sensor_type = GYRO if entry[0] == '|' else ACCEL
-            if sensor_type == GYRO:  # purposely split them up so we can handle them differently perhaps?
-                actual_data = list(eval(entry[1:]))
-                gx, gy, gz = actual_data[0], actual_data[1], actual_data[2]
-                magnitude = (gx**2 + gz**2) ** 0.5
-                gyro_acc += [magnitude]
-            else:  # accel
-                actual_data = list(eval(entry.replace('\ufeff', '')))
-                ax, ay, az = actual_data[0], actual_data[1], actual_data[2]
-                magnitude = (ax**2 + ay**2 + az**2) ** 0.5
-                accel_acc += [magnitude]
-            if WINDOW_SIZE - acc <= 1:
-                accel_mean = np.mean(accel_acc)
-                gyro_mean = np.mean(gyro_acc)
-                new_features += [accel_mean, gyro_mean]
-                
-                new_labels +=[rev_idx[folder]]
-                if features is not None:
-                    features += [new_features]
-                    labels += (new_labels)
-                else:
-                    features = new_features
-                    labels = new_labels
-                new_features, new_labels = [], []
-                acc = 0
+        if file != task or folder == 'euphoria':
+            continue
+        print(file, folder, 'wut')
+        for txt in os.listdir(train_dir+ folder + '/' +file):
+            if txt == '.DS_Store':
+                continue
+            with open(train_dir + folder + '/' + file + '/' + txt, 'r') as f:
+                string_data = f.read()
+
+            list_data = (string_data.split('\n'))
+            length = len(list_data)
+            list_data = list(filter(None, list_data))[500:len(list_data)-500]
+            new_features, new_labels = [], []
+            acc = 0
+            accel_acc, gyro_acc = [], []
+            for entry in list_data:
+                acc += 1
+                sensor_type = GYRO if entry[0] == '|' else ACCEL
+                if sensor_type == GYRO:  # purposely split them up so we can handle them differently perhaps?
+                    actual_data = list(eval(entry[1:]))
+                    try:
+                        gx, gy, gz = actual_data[0], actual_data[1], actual_data[2]
+                    except:
+                        continue
+                    magnitude = (gx**2 + gz**2) ** 0.5
+                    gyro_acc += [magnitude]
+                else:  # accel
+                    actual_data = list(eval(entry.replace('\ufeff', '')))
+                    try:
+                        ax, ay, az = actual_data[0], actual_data[1], actual_data[2]
+                    except:
+                        continue
+                    magnitude = (ax**2 + ay**2 + az**2) ** 0.5
+                    accel_acc += [magnitude]
+                if WINDOW_SIZE - acc <= 1:
+                    accel_mean = np.mean(accel_acc)
+                    gyro_mean = np.mean(gyro_acc)
+                    new_features += [accel_mean, gyro_mean]
+                    
+                    new_labels +=[rev_idx[folder]]
+                    if features is not None:
+                        features += [new_features]
+                        labels += (new_labels)
+                    else:
+                        if new_features is None or new_labels is None:
+                            print('fuck up')
+                        features = new_features
+                        labels = new_labels
+                    new_features, new_labels = [], []
+                    acc = 0
+    print(features, labels)
     return features[2:], labels[1:]
 ###################### End load and Process Data ###########################
 
@@ -65,9 +80,9 @@ Y_train = None
 X_test = None 
 Y_test = None 
 for file in os.listdir(train_dir):
-    if file == '.DS_Store' or file == 'coma':  # remove soon
+    if file == '.DS_Store' or file == 'euphoria':  # remove soon
         continue
-    features, labels = extract_features_labels(file)
+    features, labels = extract_features_labels(file, 'still')
     if X_train is None:
         if features is not None:
             X_train = np.array(features)
@@ -76,7 +91,6 @@ for file in os.listdir(train_dir):
         X_train = np.append(X_train, features, axis=0)
         Y_train = np.append(Y_train, labels, axis= 0)
 # print(X_train, Y_train)
-print(X_train)
 print(X_train.shape, Y_train.shape)
 clf = svm.SVC()
 X, y = X_train, Y_train
